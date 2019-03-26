3dee80489f03eca976d0abd79656a7da4799a57b
Surya <surya@keyba.se>
2019-03-26 14:56:46 -0400

    w

--- go/libkb/secret_store_secretservice.go
+++ go/libkb/secret_store_secretservice.go
@@ -1,6 +1,7 @@
 package libkb
 
 import (
+	"errors"
 	"fmt"
 
 	dbus "github.com/guelfey/go.dbus"
@@ -16,14 +17,38 @@ func NewSecretStoreSecretService() *SecretStoreSecretService {
 	return &SecretStoreSecretService{}
 }
 
-func (s *SecretStoreSecretService) makeAttributes(username NormalizedUsername) secsrv.Attributes {
+func (s *SecretStoreSecretService) makeServiceAttributes() secsrv.Attributes {
 	return secsrv.Attributes{
-		"username": string(username),
-		"service":  SecretServiceKeyringServiceName,
+		"service": SecretServiceKeyringServiceName,
 	}
 }
 
-func (s *SecretStoreSecretService) RetrieveSecret(m MetaContext, username NormalizedUsername) (LKSecFullSecret, error) {
+func (s *SecretStoreSecretService) makeAttributes(username NormalizedUsername) secsrv.Attributes {
+	serviceAttributes := s.makeServiceAttributes()
+	serviceAttributes["username"] = string(username)
+	return serviceAttributes
+}
+
+func (s *SecretStoreSecretService) retrieveItem(mctx MetaContext, srv *secsrv.SecretService, username NormalizedUsername) (dbus.ObjectPath, error) {
+	if srv == nil {
+		return "", fmt.Errorf("got nil d-bus secretservice")
+	}
+	items, err := srv.SearchCollection(secsrv.DefaultCollection, s.makeAttributes(username))
+	if err != nil {
+		return "", err
+	}
+	if len(items) < 1 { // and if > 1? clear all..or something
+		return "", fmt.Errorf("no secret found") // need real errtype
+	}
+	item := items[0]
+	err = srv.Unlock([]dbus.ObjectPath{item})
+	if err != nil {
+		return "", err
+	}
+	return item, nil
+}
+
+func (s *SecretStoreSecretService) RetrieveSecret(mctx MetaContext, username NormalizedUsername) (LKSecFullSecret, error) {
 	srv, err := secsrv.NewService()
 	if err != nil {
 		return LKSecFullSecret{}, err
@@ -32,18 +57,12 @@ func (s *SecretStoreSecretService) RetrieveSecret(m MetaContext, username Normal
 	if err != nil {
 		return LKSecFullSecret{}, err
 	}
-	items, err := srv.SearchCollection(secsrv.DefaultCollection, s.makeAttributes(username))
+
+	item, err := s.retrieveItem(mctx, srv, username)
 	if err != nil {
 		return LKSecFullSecret{}, err
 	}
-	if len(items) < 1 { // and if > 1?
-		return LKSecFullSecret{}, fmt.Errorf("no secret found") // need real errtype
-	}
-	item := items[0]
-	err = srv.Unlock([]dbus.ObjectPath{item})
-	if err != nil {
-		return LKSecFullSecret{}, err
-	}
+
 	secret, err := srv.GetSecret(item, session)
 	if err != nil {
 		return LKSecFullSecret{}, err
@@ -51,7 +70,7 @@ func (s *SecretStoreSecretService) RetrieveSecret(m MetaContext, username Normal
 	return newLKSecFullSecretFromBytes(secret.Value)
 }
 
-func (s *SecretStoreSecretService) StoreSecret(m MetaContext, username NormalizedUsername, secret LKSecFullSecret) error {
+func (s *SecretStoreSecretService) StoreSecret(mctx MetaContext, username NormalizedUsername, secret LKSecFullSecret) error {
 	srv, err := secsrv.NewService()
 	if err != nil {
 		return err
@@ -79,16 +98,42 @@ func (s *SecretStoreSecretService) StoreSecret(m MetaContext, username Normalize
 	return nil
 }
 
-func (s *SecretStoreSecretService) ClearSecret(m MetaContext, username NormalizedUsername) error {
-	// noop for "" username? for rest too?
+func (s *SecretStoreSecretService) ClearSecret(mctx MetaContext, username NormalizedUsername) error {
+	srv, err := secsrv.NewService()
+	if err != nil {
+		return err
+	}
+	item, err := s.retrieveItem(mctx, srv, username)
+	if err != nil {
+		return err
+	}
+	err = srv.DeleteItem(item)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-func (s *SecretStoreSecretService) GetUsersWithStoredSecrets(m MetaContext) ([]string, error) {
-	// var usernames []string
-	// for k := range s.secrets {
-	// 	usernames = append(usernames, k.String())
-	// }
-	// return usernames, nil
-	return nil, nil
+func (s *SecretStoreSecretService) GetUsersWithStoredSecrets(mctx MetaContext) ([]string, error) {
+	srv, err := secsrv.NewService()
+	if err != nil {
+		return nil, err
+	}
+	items, err := srv.SearchCollection(secsrv.DefaultCollection, s.makeServiceAttributes())
+	if err != nil {
+		return nil, err
+	}
+	var usernames []string
+	for _, item := range items {
+		attributes, err := srv.GetAttributes(item)
+		if err != nil {
+			return nil, err
+		}
+		username, ok := attributes["username"]
+		if !ok {
+			return nil, errors.New("secret does not have username key")
+		}
+		usernames = append(usernames, username)
+	}
+	return usernames, nil
 }
